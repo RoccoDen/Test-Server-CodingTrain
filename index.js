fb020ba7a56c4f60056ec2c4077ecb8bbf93ad23
Rocco <rocco.denicolo@bit2win.com>
2023-03-04 13:07:11 +0100

    server port from .env file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const Datastore = require('nedb');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 require('dotenv').config();
 console.log(process.env);
